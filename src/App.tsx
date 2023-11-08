3715428c2757dbba598f2649698e78cfdf5d5ea3
ILdar Nogmanov <nogmanov@gmail.com>
2023-11-08 16:36:48 +0300

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,43 @@
-import logo from "./logo.svg"
-import { Counter } from "./features/counter/Counter"
+import React, { useState } from "react"
+import HighchartsReact from "highcharts-react-official"
+import Highcharts from "highcharts"
 import "./App.css"
 
-function App() {
+const getNewArray = () => {
+  return Array.from({ length: 100 }, () => Math.floor(Math.random() * 100))
+}
+
+const LineChart = () => {
+  const [chartOptions, setChartOptions] = useState({
+    chart: {
+      type: "area",
+    },
+    series: [
+      {
+        data: getNewArray(),
+      },
+    ],
+  })
+
+  const updateSeries = () => {
+    setChartOptions({
+      ...chartOptions,
+      series: [
+        {
+          data: getNewArray(),
+        },
+      ],
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="container">
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+      <div className="btn">
+        <button onClick={updateSeries}>UPDATE</button>
+      </div>
     </div>
   )
 }
 
-export default App
+export default LineChart
